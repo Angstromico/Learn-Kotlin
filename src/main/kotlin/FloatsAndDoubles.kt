13088f2a26748e7f82f2c44404a0abb51e74ed80
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-06-23 11:15:16 -0400

    lear(vars): Floats and doubles

---
+++ src/main/kotlin/FloatsAndDoubles.kt
@@ -0,0 +1,9 @@
+package main.kotlin
+
+fun main() {
+    var floatNumber = 2F //Floats has more accuracy, thus doesn't need the .0
+    var doubleNumber = 2.0
+
+    println("Float: $floatNumber")
+    println("Double: $doubleNumber")
+}
