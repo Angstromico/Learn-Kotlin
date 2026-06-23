ad6fe22f1b97dbe38aa3bdfadb41bddc997e19d9
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-06-23 09:35:19 -0400

    learn(variables): Using immutable variables with 'val'

--- src/main/kotlin/Variables.kt
+++ src/main/kotlin/Variables.kt
@@ -1,13 +1,16 @@
 package main.kotlin
 
+import java.util.Locale
+
 fun main() {
     var userName: String = "Manuel Morales"
     var age: Int = 35
+    val country : String = Locale.getDefault().displayCountry
 
-    println("The user $userName has the age $age")
+    println("The user $userName has the age $age and country $country")
 
     userName = "Jose Pacheco"
     age = 30
 
-    println("The user $userName is $age")
+    println("The user $userName is $age and country is $country")
 }
