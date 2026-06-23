407c6fde4296deaf56fddfefeec3301da874cf16
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-06-23 08:10:27 -0400

    chore(main): explain with comments some basic stucks

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 package main.kotlin
 
 fun main() {
-    println("Hello Kotlin");
+    //The main function in Kotlin is mandatory in order to can execute this code
+    println("Hello Kotlin") //The semicolon is optional
 }
