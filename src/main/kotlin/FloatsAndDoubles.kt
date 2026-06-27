767427fbb20ec68a1beab832dbd5de96e89d2c42
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-06-27 09:47:54 -0400

    refactor: use val in no changing variables from floats and doubles

--- src/main/kotlin/FloatsAndDoubles.kt
+++ src/main/kotlin/FloatsAndDoubles.kt
@@ -1,8 +1,8 @@
 package main.kotlin
 
 fun main() {
-    var floatNumber = 2F //Floats has more accuracy, thus doesn't need the .0
-    var doubleNumber = 2.0
+    val floatNumber = 2F //Floats has more accuracy, thus doesn't need the .0
+    val doubleNumber = 2.0
 
     println("Float: $floatNumber")
     println("Double: $doubleNumber")
