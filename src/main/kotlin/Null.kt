5e76d7af473b9c65bb93c07560105f585d8550f3
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-06-30 10:29:07 -0400

    lear(null): check nullable value

--- src/main/kotlin/Null.kt
+++ src/main/kotlin/Null.kt
@@ -3,4 +3,10 @@ package main.kotlin
 fun main() {
     val text: String? = null //A null can be assigned to a value with '?'
     println(text)
+
+    val name: String? = "Name"
+
+    if(name?.length != 0) {
+        println(name)
+    }
 }
