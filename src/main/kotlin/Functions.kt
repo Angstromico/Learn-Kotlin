aee302a8bd7533997424cce1c806fd5ea9769786
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-06-30 15:58:11 -0400

    practice(funcs): get max number of a list

--- src/main/kotlin/Functions.kt
+++ src/main/kotlin/Functions.kt
@@ -2,8 +2,23 @@ package main.kotlin
 
 fun main() {
     println(salutations("Manuel"))
+    val numbers = listOf(1, 2, 3, 4, 5)
+    val greaterNumber = getMaxOfArr(numbers)
+
+    println("The grater number of the list $numbers is $greaterNumber")
 }
 
 fun salutations(name: String): String {
     return "Hello $name"
+}
+
+fun getMaxOfArr(numbers: List<Int>): Int {
+    var max = 0
+    for (number in numbers) {
+        if (number > max) {
+            max = number
+        }
+    }
+
+    return max
 }
