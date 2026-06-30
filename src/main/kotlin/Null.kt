d17e94144d8e3bd374d27a237d97505014223bf5
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-06-30 10:38:08 -0400

    lear(null): check nullable else condition

--- src/main/kotlin/Null.kt
+++ src/main/kotlin/Null.kt
@@ -4,9 +4,11 @@ fun main() {
     val text: String? = null //A null can be assigned to a value with '?'
     println(text)
 
-    val name: String? = "Name"
+    val name: String? = null
 
     if(name?.length != 0 || name != null) {
         println(name?.length)
+    } else {
+        println("The variable name is null")
     }
 }
