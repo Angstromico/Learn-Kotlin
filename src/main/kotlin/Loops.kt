d3e17f124e11d8cc540513200b5fa196a2bc50ee
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-09-02 09:15:29 -0400

    practice(funcs): test if printCollection can print any type

--- src/main/kotlin/Loops.kt
+++ src/main/kotlin/Loops.kt
@@ -4,6 +4,7 @@ fun main() {
     printCollection(1, 2, 3)
     printCollection("A", "B", "C")
     printCollection(true, false, true)
+    printCollection(false, true, true, 0, "Animalia")
 }
 
 fun <T> printCollection(vararg collection: T) {
