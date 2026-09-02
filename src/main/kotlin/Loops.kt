941d6bf8398c63a65d5461f3cb94e8b4d3ee9816
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-09-02 09:22:19 -0400

    practice(funcs): iterate over a given range

--- src/main/kotlin/Loops.kt
+++ src/main/kotlin/Loops.kt
@@ -5,10 +5,27 @@ fun main() {
     printCollection("A", "B", "C")
     printCollection(true, false, true)
     printCollection(false, true, true, 0, "Animalia")
+
+    iterateInRange(1,6)
+    iterateInRange(6,1)
 }
 
 fun <T> printCollection(vararg collection: T) {
     for (item in collection) {
         println(item)
+    }
+}
+
+//To iterate over a range of numbers, use a range expression with .. and ..< operators:
+
+fun iterateInRange(init: Int, end: Int) {
+    if(init >= end) {
+        println("$init is greater than $end so you can't iterate over that range")
+        return
+    }
+
+    println("Closed-ended range:")
+    for (i in init..end) {
+        println(i)
     }
 }
